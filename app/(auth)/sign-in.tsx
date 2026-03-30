38b125d50f7db8e3458ffd64db155d3500ef6a77
Silly_coder <104496990+Pritam-25@users.noreply.github.com>
2026-03-30 22:25:55 +0530

    feat: initialize app routes and NativeWind setup

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const signIn = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>sign-in</Text>
+    </View>
+  );
+}
+
+export default signIn
---
+++ app/(tabs)/insights.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const insights = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>insights</Text>
+    </View>
+  );
+}
+
+export default insights
